0ab81604d4346d410f69ead77b6f0ca6c2c3cedb
Feek <fpm5022@gmail.com>
2015-09-29 17:55:12 -0400

    front end for sending emails

--- app/js/views/HomeView.js
+++ app/js/views/HomeView.js
@@ -9,10 +9,14 @@ define([
 		template: tpl,
 
 		events: {
+			'click @ui.sendEmail' : 'sendEmail'
 		},
 
 		ui: {
-			'carousel' : '.carousel'
+			'carousel' : '.carousel',
+			'sendEmail' : '.send-email',
+			'address' : '.address', // email address
+			'message' : '.message' // email message
 		},
 
 		initialize: function(options) {
@@ -24,6 +28,41 @@ define([
 				arrows: false,
 				pauseOnHover: false
 			});
+		},
+
+		sendEmail: function(e) {
+			e.preventDefault();
+
+			if (this.validateEmail()) {
+				console.log('send');
+			}
+		},
+
+		validateEmail: function() {
+			this.clearErrors();
+
+			var addressRegex = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"); // good enough
+			var address = this.ui.address.val();
+			var message = this.ui.message.val();
+
+			if (addressRegex.test(address)) {
+				if (message && message.length > 1) {
+					return true;
+				} else {
+					// add error message
+					$('<small class="error">Please enter a message</small>').insertAfter(this.ui.message);
+				}
+			} else  {
+				// add error message
+				$('<small class="error">Please enter a valid email</small>').insertAfter(this.ui.address);
+			}
+
+			return false;
+		},
+
+		// remove previous errors, if any
+		clearErrors: function() {
+			$('.error').remove();
 		}
 	});
 
