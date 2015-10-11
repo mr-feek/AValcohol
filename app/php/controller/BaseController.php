a43384a2476084c4fed3ca82f05d3116c12b82eb
Feek <fpm5022@gmail.com>
2015-10-11 19:54:21 -0400

    implement seed_one function in base controller for seeding models

--- app/php/controller/BaseController.php
+++ app/php/controller/BaseController.php
@@ -7,8 +7,24 @@
  */
 
 namespace Controller;
+use Models\User;
 
 abstract class BaseController
 {
+	/*
+	 * creates a fake model and saves it to the database. does not return anything
+	 * right now. I HAVE CHANGED A LINE INSIDE Seeder.php to make this work
+	 */
+	public static function seed_one() {
+		// will be something like "Controller\\UserController"
+		$fullClassName = get_called_class();
 
+		// strips namespace. will be something like UserController
+		$shortClassName = substr($fullClassName, strrpos($fullClassName, '\\') + 1);
+
+		// remove the last ten characters (Controller). will be something like User
+		$modelName = substr($shortClassName, 0, -10);
+
+		return \Seeder::seed($modelName);
+	}
 }
