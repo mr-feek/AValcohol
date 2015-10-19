9494ca02fd93456638d5365a6dcc9bfa7e46cd26
Feek <fpm5022@gmail.com>
2015-10-19 15:30:44 -0400

    sidebar links now affect routing

--- js/views/ProductCategoriesView.js
+++ js/views/ProductCategoriesView.js
@@ -1,16 +1,32 @@
 define([
 	'marionette',
+	'App',
 	'tpl!templates/product-categories.html'
-], function (Mn,
-			 tpl) {
+], function (
+	Mn,
+	App,
+	tpl
+) {
 	var ProductCategoriesView = Mn.ItemView.extend({
 		template: tpl,
 
-		events: {},
+		events: {
+			'click @ui.link' : 'linkClicked'
+		},
 
-		ui: {},
+		ui: {
+			link: 'a'
+		},
 
 		initialize: function (options) {
+			this.router = App.router;
+		},
+
+		linkClicked: function(e) {
+			e.preventDefault();
+			var link =  e.target.innerHTML.toLowerCase();
+			var endpoint = 'home/' + link;
+			this.router.navigate(endpoint, {trigger: true});
 		}
 	});
 
