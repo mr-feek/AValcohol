ffb9592c5d9b6fe3ff45737a86a233a59390df16
Feek <fpm5022@gmail.com>
2015-12-02 00:26:33 -0500

    build sass

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 if ([ "$TRAVIS_BRANCH" == "master" ]) &&
 [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	npm install;
+	gem install sass;
+	sass --update sass:css;
     gulp deploy --user $FTPUSER --password $FTP_PASSWORD;
 else
     echo "not deploying because branch is not master."
