1cff0ac051c19dcafe5bcc9bf5efae73af5f0449
Feek <fpm5022@gmail.com>
2015-12-02 02:24:34 -0500

    include htaccess?

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task('deploy', function() {
 			if (i == globs.length) {
 				// by now, all the directories we want to remove have been removed. time to upload!
 				// done this way because of concurrency issues
-				return gulp.src( globs, { base: '.', buffer: false } )
+				return gulp.src( globs, { base: '.', buffer: false, dot: true } )
 					.pipe( conn.newer( '/public_html/dev' ) ) // only upload newer files
 					.pipe( conn.dest( '/public_html/dev' ) );
 			}
