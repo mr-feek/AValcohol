8f09250513e5c7730e8f61e42ed80fd56d4da8a3
Feek <fpm5022@gmail.com>
2015-12-20 00:48:21 -0500

    removed pusher service provider and cycling keys

--- app/Listeners/OrderWasSubmittedListener.php
+++ app/Listeners/OrderWasSubmittedListener.php
@@ -11,26 +11,9 @@ namespace App\Listeners;
 
 use App\Events\OrderWasSubmitted;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Pusher;
 
 class OrderWasSubmittedListener extends Listener implements ShouldQueue
 {
-	protected $pusher;
-
-	public function __construct(Pusher $pusher)
-	{
-		$this->pusher = $pusher;
-	}
-
 	public function handle(OrderWasSubmitted $event) {
-		// submit to pusher
-		$order = $event->order; // THIS DOES NOT HAVE RELATIONSHIPS FETCHED
-		$order->user = $order->user();
-
-		$channel = $event->broadcastOn();
-		$eventName = 'new_order';
-		$message = $event->broadcastsWith();
-
-		//$this->pusher->trigger($channel, $eventName, $message);
 	}
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,12 +14,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-		$this->app->singleton('Pusher', function ($app) {
-			$app_id = '161104';
-			$app_key = 'c644fb6bfa1ca7b73b06';
-			$app_secret = '6e5f3cb426a007049e8c';
-
-			return new Pusher($app_key, $app_secret, $app_id);
-		});
     }
 }
