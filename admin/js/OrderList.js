0494beb08bdbe372ada047d60492f9fa85a755fb
Feek <fpm5022@gmail.com>
2015-12-23 02:28:00 -0500

    basic styling of order list

--- admin/js/OrderList.js
+++ admin/js/OrderList.js
@@ -2,39 +2,41 @@ var OrderList = React.createClass({
 	render: function() {
 		var list = this.props.orders.map(function(order){
 			return  [
-				<li>Order ID: {order.id}</li>,
-				<ul>
-					Product Details
+				<div className="order panel">
+					<li>Order ID: {order.id}</li>
 					<ul>
-						<li>Product Name: {order.product.name}</li>
-						<li>Contains: {order.product.contains} {order.product.container}(s) {order.product.ounces}oz</li>
-					</ul>
+						Product Details
+						<ul>
+							<li>Product Name: {order.product.name}</li>
+							<li>Contains: {order.product.contains} {order.product.container}(s) {order.product.ounces}oz</li>
+						</ul>
 
-					Order Details
-					<ul>
-						<li>Order Status: {order.status}</li>
-						<li>Time Placed: {order.created_at}</li>
-						<li>Amount Paid: {order.amount}</li>
-					</ul>
+						Order Details
+						<ul>
+							<li>Order Status: {order.status}</li>
+							<li>Time Placed: {order.created_at}</li>
+							<li>Amount Paid: {order.amount}</li>
+						</ul>
 
-					User Details
-					<ul>
-						<li>Name: {order.user.first_name} {order.user.last_name}</li>
-						<li>Phone Number: {order.user.phone_number}</li>
-						<li>Email: {order.user.email}</li>
-					</ul>
+						User Details
+						<ul>
+							<li>Name: {order.user.first_name} {order.user.last_name}</li>
+							<li>Phone Number: {order.user.phone_number}</li>
+							<li>Email: {order.user.email}</li>
+						</ul>
 
-					Delivery Address
-					<ul>
-						<li>{order.address.street}, {order.address.city}, {order.address.state} ({order.address.zipcode})</li>
-					</ul>
+						Delivery Address
+						<ul>
+							<li>{order.address.street}, {order.address.city}, {order.address.state} ({order.address.zipcode})</li>
+						</ul>
 
-					Actions
-					<ul>
-						<li><a onClick={this.props.updateStatus.bind(null, order, 'out-for-delivery')} href="#">Mark as out for delivery</a></li>
-						<li><a onClick={this.props.updateStatus.bind(null, order, 'delivered')} href="#">Mark as delivered</a></li>
+						Actions
+						<ul>
+							<li><a onClick={this.props.updateStatus.bind(null, order, 'out-for-delivery')} className="button" href="#">Mark as out for delivery</a></li>
+							<li><a onClick={this.props.updateStatus.bind(null, order, 'delivered')} className="button" href="#">Mark as delivered</a></li>
+						</ul>
 					</ul>
-				</ul>
+				</div>
 			]
 		}, this);
 
