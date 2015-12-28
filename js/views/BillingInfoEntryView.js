08b31427d2c4cc145e4a998f1f09286a41f34e81
Feek <fpm5022@gmail.com>
2015-12-28 04:26:21 -0500

    migrated stripe logic out of billing info view and into checkout ivew

--- js/views/BillingInfoEntryView.js
+++ js/views/BillingInfoEntryView.js
@@ -10,49 +10,7 @@ define([
 		tagName: 'form',
 		className: 'payment-form',
 
-		events: {
-			'submit' : 'getStripeToken'
-		},
-
 		initialize: function (options) {
-		},
-
-		/**
-		 * Stops the form from being submitted and sends the required details to stripe to authorize a token
-		 * disables the submit button
-		 * Calls stripeResponseHandler
-		 * @param evt
-		 */
-		getStripeToken: function(evt) {
-			evt.preventDefault();
-			var $form = this.$el;
-
-			// Disable the submit button to prevent repeated clicks
-			var $button = $form.find('button');
-			$button.prop('disabled', true);
-
-			Stripe.card.createToken($form.context, this.stripeResponseHandler.bind(this));
-
-			// Prevent the form from submitting with the default action
-			return false;
-		},
-
-		/**
-		 * handler for getting the stripe token
-		 * @param status
-		 * @param response
-		 */
-		stripeResponseHandler: function(status, response) {
-			var $form = this.$el;
-
-			if (response.error) {
-				// Show the errors on the form
-				$form.find('.payment-errors').text(response.error.message);
-				$form.find('button').prop('disabled', false);
-			} else {
-				// response contains id and card, which contains additional card details
-				var token = response.id;
-			}
 		}
 	});
 
--- js/views/CheckoutView.js
+++ js/views/CheckoutView.js
@@ -8,6 +8,7 @@ define([
 	'models/Product',
 	'models/User',
 	'models/UserAddress',
+	'models/Order',
 	'tpl!templates/checkout.html'
 ], function (
 	Mn,
@@ -19,6 +20,7 @@ define([
 	Product,
 	User,
 	UserAddress,
+	Order,
 	tpl
 ) {
 	var CheckoutView = Mn.LayoutView.extend({
@@ -27,19 +29,18 @@ define([
 		className: 'small-12 columns',
 
 		templateHelpers: function() {
-			var view = this;
-
 			return {
 				cart: App.cart
 			}
 		},
 
 		events: {
-			'click @ui.order' : 'submitOrder'
+			'click @ui.order' : 'getStripeToken'
 		},
 
 		ui: {
-			'order' : '.order'
+			'order' : '.order',
+			'billingForm' : '.billing-info'
 		},
 
 		regions: {
@@ -56,18 +57,17 @@ define([
 			this.getRegion('items').show(new CheckoutProductsView({ collection: App.cart }));
 
 			// if cart is empty, don't show anything else. Let checkoutproductsview emptyview handle it
-			if (App.cart.length > 0) {
+			//if (App.cart.length > 0) {
 				this.getRegion('deliveryInfo').show(new AddressEntryView());
 				this.getRegion('billingInfo').show(new BillingInfoEntryView());
-			}
+			//}
 		},
 
 		/**
-		 * THIS SHOULD BE REFACTORED TO USE AN ORDER MODEL BUt yoLO
+		 * creates a new order model and saves it to the backend
+		 * @param token verified stripe token
 		 */
-		submitOrder: function() {
-			var view = this;
-
+		submitOrder: function(token) {
 			var product = new Product({
 				id: 1 // test
 			});
@@ -80,20 +80,58 @@ define([
 				id: 1 // test
 			});
 
-			$.ajax({
-				url: '/api/order/create',
-				type: 'POST',
-				dataType: 'json',
-				data: {
-					product_id: product.get('id'),
-					user_id: user.get('id'),
-					address_id: userAddress.get('id')
-				}
-			}).done(function (result) {
+			var order = Order.findOrCreate({
+				products: product,
+				user: user,
+				address: userAddress,
+				stripe_token: token
+			});
+
+			console.log('savign order');
+
+			order.save().done(function (result) {
 				console.log('pass');
 			}).fail(function (result) {
 				console.log('fail');
 			});
+		},
+
+		/**
+		 * Stops the form from being submitted and sends the required details to stripe to authorize a token
+		 * disables the submit button
+		 * Calls stripeResponseHandler
+		 * @param evt
+		 */
+		getStripeToken: function(evt) {
+			console.log('get stripe token');
+			evt.preventDefault();
+			var $form = this.ui.billingForm;
+
+			// Disable the submit button to prevent repeated clicks
+			var $button = $form.find('button');
+			$button.prop('disabled', true);
+
+			Stripe.card.createToken($form.context, this.stripeResponseHandler.bind(this));
+		},
+
+		/**
+		 * handler for getting the stripe token
+		 * @param status
+		 * @param response
+		 */
+		stripeResponseHandler: function(status, response) {
+			console.log('stripe response handler');
+			var $form = this.ui.billingForm;
+
+			if (response.error) {
+				// Show the errors on the form
+				$form.find('.payment-errors').text(response.error.message);
+				$form.find('button').prop('disabled', false);
+			} else {
+				// response contains id and card, which contains additional card details
+				var token = response.id;
+				this.submitOrder(token);
+			}
 		}
 	});
 
