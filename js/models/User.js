c6b9f89d08d46f06d524fd6eb5de8a21ae22f606
Feek <fpm5022@gmail.com>
2015-12-28 03:17:54 -0500

    basic relations

--- js/models/User.js
+++ js/models/User.js
@@ -1,11 +1,23 @@
 define([
 	'backbone',
+	'models/Order',
 	'backboneRelational'
-], function (Backbone) {
-	var User = Backbone.Model.extend({
+], function (
+	Backbone,
+	Order
+) {
+	var User = Backbone.RelationalModel.extend({
 		urlRoot: '/api/user/',
 
-
+		/*
+		relations: [
+			{
+				type: Backbone.HasMany,
+				key: 'previous_orders',
+				relatedModel: 'Order'
+			}
+		],
+		*/
 
 		defaults: {
 			mvp_user: 1 // this account does NOT need a password, email, etc
