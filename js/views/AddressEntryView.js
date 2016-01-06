137c1d46ce69d769d06edf96dafd62a66b1b204c
Feek <fpm5022@gmail.com>
2016-01-06 01:23:48 -0500

    if user address was already submitted, autofill the information into the delivery information form. **HACK** to get address relation properly from user. not sure what to do

--- js/views/AddressEntryView.js
+++ js/views/AddressEntryView.js
@@ -1,8 +1,12 @@
 define([
 	'marionette',
+	'App',
+	'models/UserAddress',
 	'tpl!templates/address-entry.html'
 ], function (
 	Mn,
+	App,
+	UserAddress,
 	tpl
 ) {
 	var view = Mn.ItemView.extend({
@@ -12,9 +16,42 @@ define([
 
 		templateHelpers: function() {
 			var view = this;
-
 			return {
+				first: function() {
+					if (!view.model) {	return;	}
+					var val = view.model.get('first');
+					return val ? val : '';
+				},
 
+				last: function() {
+					if (!view.model) {	return;	}
+					var val = view.model.get('last');
+					return val ? val : '';
+				},
+
+				street: function() {
+					if (!view.model) {	return;	}
+					var val = view.model.get('street');
+					return val ? val : '';
+				},
+
+				city: function() {
+					if (!view.model) {	return;	}
+					var val = view.model.get('city');
+					return val ? val : '';
+				},
+
+				state: function() {
+					if (!view.model) {	return;	}
+					var val = view.model.get('state');
+					return val ? val : '';
+				},
+
+				zipcode: function() {
+					if (!view.model) {	return;	}
+					var val = view.model.get('zipcode');
+					return val ? val : '';
+				}
 			}
 		},
 
@@ -22,7 +59,15 @@ define([
 
 		ui: {},
 
-		initialize: function (options) {
+		initialize: function () {
+			var address = App.user.get('address')
+
+			// hack to get to the address.get(key) through the relation, not sure what im doing wrong otherwise...
+			if (address) {
+				address = address.attributes;
+			}
+
+			this.model = address;
 		}
 	});
 
