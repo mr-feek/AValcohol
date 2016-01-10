fd464a1179da6489f52007f34c9b493db194018a
Feek <fpm5022@gmail.com>
2016-01-09 20:34:17 -0500

    user address validation and parse

--- js/models/UserAddress.js
+++ js/models/UserAddress.js
@@ -14,11 +14,31 @@ define([
 			zipcode: undefined
 		},
 
+		parse: function(response, xhr) {
+			return response.address;
+		},
+
+		relations: {
+
+		},
+
+		required: ['city', 'street', 'state'], // taking out zipcode for now since it has additional validator
+
 		validate: function(attrs, options) {
 			var errors = [];
-			if (attrs.zipcode !== 16801 && attrs.zipcode !== "16801") {
+
+			_.each(this.required, function(attribute) {
+				if (!attrs[attribute]) {
+					errors.push({
+						attribute: attribute,
+						message: "This field is required."
+					});
+				}
+			});
+
+			if (!attrs.zipcode || (attrs.zipcode !== 16801 && attrs.zipcode !== "16801")) {
 				errors.push({
-					attribute: attrs.zipcode,
+					attribute: 'zipcode',
 					message: "We're sorry, but at this time we can only deliver to the 16801 area"
 				});
 			}
