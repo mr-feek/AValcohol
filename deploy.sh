99d9a5d02b8c04ae453084d276740abf733dd518
Feek <fpm5022@gmail.com>
2016-01-18 15:09:16 -0500

    deploy?

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,14 @@
 if ([ "$TRAVIS_BRANCH" == "master" ]) &&
 [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	sass --update sass:css;
+	rm -rf .git
 	git config user.name "Travis CI"
 	git config user.email "feek-travis@avalcohol.com"
 	git config --global push.default simple
 	git add . -f
 	git commit -m "built with love by travis <3"
-	git push "https://feeekkk:${GH_TOKEN}@github.com/feeekkk/AValcohol-production.git" master --force --quiet
+	git remote add production "https://${GH_TOKEN}@github.com/feeekkk/AValcohol-production.git"
+	git push production --force --quiet
 else
 	if ([ "$TRAVIS_BRANCH" == "dev" ]) &&
 	[ "$TRAVIS_PULL_REQUEST" == "false" ]; then
