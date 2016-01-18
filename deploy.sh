8b216a5cd76792ce58d9b9158966f1d50f7f338b
Feek <fpm5022@gmail.com>
2016-01-18 15:01:44 -0500

    deploy?

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@ if ([ "$TRAVIS_BRANCH" == "master" ]) &&
 	sass --update sass:css;
 	git config user.name "Travis CI"
 	git config user.email "feek-travis@avalcohol.com"
+	git config --global push.default simple
 	git add . -f
 	git commit -m "built with love by travis <3"
-	git push "https://feeekkk:${GH_TOKEN}@github.com/feeekkk/AValcohol-production.git" --force --quiet
+	git push "https://feeekkk:${GH_TOKEN}@github.com/feeekkk/AValcohol-production.git" master --force --quiet
 else
 	if ([ "$TRAVIS_BRANCH" == "dev" ]) &&
 	[ "$TRAVIS_PULL_REQUEST" == "false" ]; then
