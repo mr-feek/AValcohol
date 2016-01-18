42eee87bd0b8ed17e2c3813891b6067661e413ae
Feek <fpm5022@gmail.com>
2016-01-18 14:25:50 -0500

    if branch is master, have travis commit everything including built stuff to avalcohol-production

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-if ([ "$TRAVIS_BRANCH" == "dev" ]) &&
+if ([ "$TRAVIS_BRANCH" == "master" ]) &&
 [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	sass --update sass:css;
-    gulp deploy --user $FTPUSER --password $FTP_PASSWORD;
+	git remote add production git@github.com:feeekkk/AValcohol-production.git
+	git add . -f
+	git commit -m "build"
+	git push
 else
-    echo "not deploying because branch is not dev."
+	if ([ "$TRAVIS_BRANCH" == "dev" ]) &&
+	[ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+		sass --update sass:css;
+		gulp deploy --user $FTPUSER --password $FTP_PASSWORD;
+	else
+		echo "not deploying because branch is not dev."
+	fi
 fi
