b1018fe83329e99382f3a35562a9529d20bdc79f
Feek <fpm5022@gmail.com>
2016-04-01 16:55:30 -0400

    products in orders

--- vendors/js/views/OrderView.js
+++ vendors/js/views/OrderView.js
@@ -3,22 +3,25 @@
  */
 define([
 	'marionette',
+	'views/ProductView',
 	'tpl!templates/order.html'
 ], function (
 	Mn,
+	ProductView,
 	tpl
 ) {
-	var view = Mn.ItemView.extend({
+	var view = Mn.CompositeView.extend({
 		template: tpl,
 		tagName: 'div',
 		className: '',
+		childView: ProductView,
+		childViewContainer: '.products',
 
 		templateHelpers: function() {
-			var view = this;
-
+			var user = this.model.get('user');
 			return {
-				user_name: this.model.get('user').first + ' ' + this.model.get('user').last,
-				dob: this.model.get('user').dob,
+				userName: user.get('first_name') + ' ' + user.get('last_name'),
+				dob: user.get('dob'),
 				orderNumber: this.model.get('id'),
 				timePlaced: this.model.get('created_at')
 			}
@@ -29,6 +32,7 @@ define([
 		ui: {},
 
 		initialize: function (options) {
+			this.collection = this.model.get('products');
 		}
 	});
 
---
+++ vendors/js/views/ProductView.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Feek on 3/30/16.
+ */
+define([
+	'marionette',
+	'tpl!templates/product.html'
+], function (
+	Mn,
+	tpl
+) {
+	var view = Mn.ItemView.extend({
+		template: tpl,
+		tagName: 'div',
+		className: 'row',
+
+		templateHelpers: function() {
+			var view = this;
+
+			return {
+
+			}
+		},
+
+		events: {},
+
+		ui: {},
+
+		initialize: function (options) {
+		}
+	});
+
+	return view;
+});
