e6d3cc07517a2bfcdbe16627e02a01910e65da01
Feek <fpm5022@gmail.com>
2016-04-26 16:39:07 -0400

    add mysql_upgrade to longer version

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -16,3 +16,4 @@ wget http://dev.mysql.com/get/mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
 sudo dpkg --install mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
 sudo apt-get update -q
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server
+sudo mysql_upgrade
