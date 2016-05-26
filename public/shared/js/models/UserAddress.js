b9c0a73652e4bfc8e210645adff440c2b2671d7f
Feek <fpm5022@gmail.com>
2016-05-25 20:37:35 -0400

    fix #219

--- public/shared/js/models/UserAddress.js
+++ public/shared/js/models/UserAddress.js
@@ -6,6 +6,7 @@ define([
 ) {
 	var UserAddress = Backbone.RelationalModel.extend({
 		urlRoot: '/api/address',
+		propertiesToPersist: ['city', 'street', 'state', 'zipcode', 'delivery_zone_id'], // properties that will be saved in session storage
 
 		defaults: {
 			city: undefined,
@@ -25,6 +26,45 @@ define([
 
 		initialize: function() {
 			_.bindAll(this, 'getDeliveryZone');
+			this.loadFromStorage();
+			_.each(this.propertiesToPersist, function(key) {
+				this.on('change:' + key, this.attributeChanged, this);
+			}, this)
+			//this.on('change', this.persist, this); // whenever an attribute is changed throughout the app, persist
+		},
+
+		/**
+		 * helper function for logic to persist changed attribute
+		 */
+		attributeChanged: function(model) {
+			_.each(model.changed, function(value, key) {
+				this.persist(key, value);
+			}, this);
+		},
+
+		/**
+		 * attempts to load all attributes that should be in storage
+		 */
+		loadFromStorage: function() {
+			_.each(this.propertiesToPersist, function(key) {
+				this.set(key, this.retrieve(key), {	silent: true });
+			}, this);
+		},
+
+		/**
+		 * retrieves the key
+		 * @param key
+		 * @return value
+		 */
+		retrieve: function(key) {
+			return window.sessionStorage.getItem(key);
+		},
+
+		/**
+		 * Persists given key value into storage
+		 */
+		persist: function(key, value) {
+			window.sessionStorage.setItem(key, value);
 		},
 
 		required: ['city', 'street', 'state', 'zipcode'],
