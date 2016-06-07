fc006f0577901312636647a600581e27f712a21d
Feek <fpm5022@gmail.com>
2016-06-07 18:14:41 -0400

    remove pointless bindings for repositories since they arent interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -59,81 +59,45 @@ class AppServiceProvider extends ServiceProvider
 		{
 			return new UserRepository(new User(), new MailChimp(env('MAILCHIMP_KEY')));
 		});
-		$this->app->bind('UserService', function($app)
-		{
-			return new UserService($app->make('Repositories\Interfaces\UserInterface'));
-		});
 
 		$this->app->bind('App\Http\Repositories\Interfaces\UserAddressInterface', function($app)
 		{
 			return new UserAddressRepository(new UserAddress());
-		});
-		$this->app->bind('UserAddressService', function($app)
-		{
-			return new UserAddressService($app->make('Repositories\Interfaces\UserAddressInterface'));
 		});
 
 		$this->app->bind('App\Http\Repositories\Interfaces\BlacklistedAddressInterface', function($app)
 		{
 			return new BlacklistedAddressRepository(new BlacklistedAddress());
 		});
-		$this->app->bind('BlacklistedAddressService', function($app)
-		{
-			return new BlacklistedAddressService($app->make('Repositories\Interfaces\BlacklistedAddressInterface'));
-		});
 
 		$this->app->bind('App\Http\Repositories\Interfaces\OrderInterface', function($app)
 		{
 			return new OrderRepository(new Order());
-		});
-		$this->app->bind('OrderService', function($app)
-		{
-			return new OrderService($app->make('Repositories\Interfaces\OrderInterface'));
 		});
 
 		$this->app->bind('App\Http\Repositories\Interfaces\ProductInterface', function($app)
 		{
 			return new ProductRepository(new Product());
 		});
-		$this->app->bind('ProductService', function($app)
-		{
-			return new ProductService($app->make('Repositories\Interfaces\ProductInterface'));
-		});
 
 		$this->app->bind('App\Http\Repositories\Interfaces\VendorInterface', function($app)
 		{
 			return new VendorRepository(new Vendor());
-		});
-		$this->app->bind('VendorService', function($app)
-		{
-			return new VendorService($app->make('Repositories\Interfaces\VendorInterface'));
 		});
 
 		$this->app->bind('App\Http\Repositories\Interfaces\AdminInterface', function($app)
 		{
 			return new AdminRepository();
 		});
-		$this->app->bind('AdminService', function($app)
-		{
-			return new AdminService($app->make('Repositories\Interfaces\AdminInterface'));
-		});
 
 		$this->app->bind('App\Http\Repositories\Interfaces\OrderStatusInterface', function($app)
 		{
 			return new OrderStatusRepository(new OrderStatus());
 		});
-		$this->app->bind('OrderStatusService', function($app)
-		{
-			return new OrderStatusService($app->make('Repositories\Interfaces\OrderStatusInterface'));
-		});
 
 		$this->app->bind('App\Http\Repositories\Interfaces\OrderDeliveryDetailsInterface', function($app)
 		{
 			return new OrderDeliveryDetailsRepository(new OrderDeliveryDetail());
-		});
-		$this->app->bind('OrderDeliveryDetailsService', function($app)
-		{
-			return new OrderDeliveryDetailsService($app->make('Repositories\Interfaces\OrderDeliveryDetailsInterface'));
 		});
 	}
 }
