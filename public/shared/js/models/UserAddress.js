6b405f37e0e22146780c6f744772b0d83202ca96
Feek <fpm5022@gmail.com>
2016-12-10 13:26:59 -0800

    extract session storage mixin and integrate with address model

--- public/shared/js/models/UserAddress.js
+++ public/shared/js/models/UserAddress.js
@@ -1,14 +1,14 @@
 define([
 	'backbone',
-	'shared/js/util/StorageHelper',
-	'backboneRelational',
+	'shared/js/util/SessionStorageMixin',
+	'backboneRelational'
 ], function (
 	Backbone,
-	StorageHelper
+	SessionStorageMixin
 ) {
-	var UserAddress = Backbone.RelationalModel.extend({
+	var UserAddress = Backbone.RelationalModel.extend(_.extend(SessionStorageMixin, {
 		urlRoot: '/api/address',
-		propertiesToPersist: ['city', 'street', 'state', 'zipcode', 'delivery_zone_id', 'location', 'apartment_number'], // properties that will be saved in session storage
+		sessionAttributes: ['city', 'street', 'state', 'zipcode', 'delivery_zone_id', 'location', 'apartment_number'],
 
 		defaults: {
 			city: null,
@@ -31,14 +31,10 @@ define([
 			options = options || {};
 			
 			_.bindAll(this, 'getDeliveryZone');
-			this.storageHelper = new StorageHelper();
 
 			// flag for if this address should be stored / load from storage. IE this should only be used for the local user, not for loading addresses in orders
-			if (options.useStorage && this.storageHelper.storageAvailable) {
-				this.loadFromStorage();
-				_.each(this.propertiesToPersist, function(key) {
-					this.on('change:' + key, this.attributeChanged, this);
-				}, this)
+			if (options.useStorage) {
+				this.initializeSessionStorage();
 			}
 		},
 
@@ -52,65 +48,6 @@ define([
 			return this.get('street') + ' ' + this.get('city') + ' ' + this.get('zipcode') + apartmentNumberDisplay;
 		},
 
-		/**
-		 * helper function for logic to persist changed attribute
-		 */
-		attributeChanged: function(model) {
-			_.each(model.changed, function(value, key) {
-				// only persist if this key is in the array of keys to persist
-				if (_.contains(this.propertiesToPersist, key)) {
-					// if its an object then change the key name so it can be properly persisted ( delimited by '.')
-					if (_.isObject(value)) {
-						// cycle through the keys of the supplied object so that they are all stored properly
-						_.each(_.keys(value), function(keyName) {
-							var nameToPersist = key + '.' + keyName;
-							var valueToPersist = value[keyName];
-							this.persist(nameToPersist, valueToPersist);
-						}, this);
-						return;
-					}
-
-					this.persist(key, value);
-				}
-			}, this);
-		},
-
-		/**
-		 * attempts to load all attributes that should be in storage
-		 */
-		loadFromStorage: function() {
-			_.each(this.propertiesToPersist, function(key) {
-				this.set(key, this.retrieve(key), {	silent: true });
-			}, this);
-		},
-
-		/**
-		 * retrieves the key
-		 * @param key
-		 * @return value
-		 */
-		retrieve: function(key) {
-			// location needs to be handled differently since it is an object
-			if (key === 'location') {
-				var longitude = window.sessionStorage.getItem('location.longitude');
-				var latitude = window.sessionStorage.getItem('location.latitude');
-
-				return {
-					longitude: longitude,
-					latitude: latitude
-				}
-			}
-
-			return this.storageHelper.getItem(key);
-		},
-
-		/**
-		 * Persists given key value into storage
-		 */
-		persist: function(key, value) {
-			this.storageHelper.setItem(key, value);
-		},
-
 		required: ['city', 'street', 'state', 'zipcode'],
 
 		validate: function(attrs, options) {
@@ -121,7 +58,7 @@ define([
 					var message = 'This field is required.';
 
 					if (attribute == 'zipcode') {
-						var message = 'A valid US zipcode is required to use our service.';
+						message = 'A valid US zipcode is required to use our service.';
 					}
 
 					errors.push({
@@ -152,8 +89,28 @@ define([
 				}
 			}.bind(this));
 			return promise;
+		},
+
+		/**
+		 * retrieves the key. overrides session mixin
+		 * @param key
+		 * @return value
+		 */
+		getFromStorage: function(key) {
+			// location needs to be handled differently since it is an object
+			if (key === 'location') {
+				var longitude = this.storage.getItem('location.longitude');
+				var latitude = this.storage.getItem('location.latitude');
+
+				return {
+					longitude: longitude,
+					latitude: latitude
+				}
+			}
+
+			return this.storage.getItem(key);
 		}
-	});
+	}));
 
 	return UserAddress;
 });
