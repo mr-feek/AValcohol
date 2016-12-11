5f7818ec5d2bfc96e84a2950b3ab29019305e03f
Feek <fpm5022@gmail.com>
2016-12-11 12:03:28 -0800

    make data store private

--- public/shared/js/Brain.js
+++ public/shared/js/Brain.js
@@ -1,21 +1,21 @@
 define([], function () {
     var Brain = {
-        dataStore: [],
+        _dataStore: [],
 
         retrieve: function(key) {
-            if (!this.dataStore[key]) {
+            if (!this._dataStore[key]) {
                 console.error('retrieving a non-existant key from the data store: ' + key);
             }
 
-            return this.dataStore[key];
+            return this._dataStore[key];
         },
 
         store: function(key, value) {
-            if (this.dataStore[key]) {
+            if (this._dataStore[key]) {
                 console.warn('overwriting key in the datastore: ' + key);
             }
 
-            this.dataStore[key] = value;
+            this._dataStore[key] = value;
         }
     };
 
