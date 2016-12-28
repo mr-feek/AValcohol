0a6bbb20828d16e019db5eec00280c9a56976815
Feek <fpm5022@gmail.com>
2016-12-28 02:33:42 -0500

    implement overwrite in brain

--- public/shared/js/Brain.js
+++ public/shared/js/Brain.js
@@ -21,6 +21,7 @@ define([], function () {
          * @param value
          * @param options
          *  - ignoreWarning
+         *  @returns {Brain}
          */
         store: function(key, value, options) {
             options = options || {};
@@ -29,6 +30,44 @@ define([], function () {
             }
 
             this._dataStore[key] = value;
+
+            return this;
+        },
+
+		/**
+         * overwrites the given key in the store, locally deleting the model
+         *
+         * @param key
+         * @param value
+         * @param options
+         * @returns {*}
+         */
+        overwrite: function(key, value, options) {
+            if (!this.retrieve(key)) {
+                // treat it as a store
+                return this.store(key, value, options)
+            }
+
+            return this.forget(key).store(key, value, options);
+        },
+
+		/**
+		 * forgets a model from the store, calling a local delete function if it exists
+         * @param key
+         * @returns {Brain}
+         */
+        forget: function(key) {
+            var old = this.retrieve(key);
+
+            // delete the old model locally
+            if (typeof old.destroyClientSide === 'function') {
+                old.destroyClientSide();
+            }
+
+            // clear the store
+            delete this._dataStore[key];
+
+            return this;
         }
     };
 
